4af5e9629b5557ae5af0d2f49a333b259bb3f937
adhithyaadhi1003 <adhithya1003@gmail.com>
2021-11-12 07:56:14 -0800

    initial commit

---
+++ LineLength.java
@@ -0,0 +1,46 @@
+package com.LineLength;
+
+public class LineLength {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+			System.out.println("  to calculate a Line length");
+			
+			double neg = Math.floor(Math.random() * 2);		
+			double x1 = Math.floor(Math.random() * 10);
+			
+			if (neg == 1) {
+				x1 *= -1;
+			}
+			
+			neg = Math.floor(Math.random() * 2);
+			double y1 = Math.floor(Math.random() * 10);
+			if (neg == 1) {
+				y1 *= -1;
+			}
+			
+			neg = Math.floor(Math.random() * 2);
+			double x2 = Math.floor(Math.random() * 10);
+			if (neg == 1) {
+				x2 *= -1;
+			}
+			
+			neg = Math.floor(Math.random() * 2);
+			double y2 = Math.floor(Math.random() * 10);
+			if (neg == 1) {
+				y2 *= -1;
+			}
+			
+			double length = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+			
+			System.out.println(" The first point is  (" + x1 + ", " + y1 + ")");
+			System.out.println(" The  Second point is  (" + x2 + ", " + y2 + ")");
+			System.out.println(" The length of the line is  " + length);
+	}
+
+	
+
+}
+
+
